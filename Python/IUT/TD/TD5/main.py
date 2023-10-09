a1e17f1803326c1e67a757c01f56e35048c84901
VictorGrosset_Surface <grosset.victor@hotmail.Fr>
2023-10-09 09:45:43 +0200

    Fin TD5 POO

--- Python/IUT/TD/TD5/main.py
+++ Python/IUT/TD/TD5/main.py
@@ -1,6 +1,10 @@
 import time
 import random
 
+class Piece:
+    def __init__(self, numero, surface):
+        self.surface = 0
+        self.numero = 0
 
 # class CapteurPresence(Subject):
 #     def __init__(self, seuil: int):
@@ -16,49 +20,60 @@ import random
 #         else:
 #             pass
 class Observer:
-    def update(self, detection: bool):
+    def update(self, detection: bool, salle):
         pass
 
 
 # observer
 class AppIoT(Observer):
-    def update(self, detection: bool):
+    def update(self, detection: bool, salle):
         if detection:
-            print(f"Présence détectée !")
+            print(f"Présence détectée dans {salle.numero}!")
+
+
+class AppSecu(Observer):
+    def update(self, detection: bool, salle):
+        if detection:
+            print(f"Alerte dans {salle.numero}")
 
 
 class Subject:
     def __init__(self):
-        self.observers = []
+        self._observers = []
 
     def addObserver(self, observer):
-        self.observers.append(observer)
+        self._observers.append(observer)
 
     def removeObserver(self, observer):
-        self.observers.remove(observer)
+        self._observers.remove(observer)
 
-    def notifyObservers(self, detection: bool):
-        for observer in self.observers:
-            observer.update(detection)
+    def notifyObservers(self, detection: bool, salle: Piece):
+        for observer in self._observers:
+            observer.update(detection, salle)
 
 
 class CapteurPresence(Subject):
-    def __init__(self, seuil):
+    def __init__(self, seuil, salleAssociee):
         super().__init__()
         self.seuil = seuil
         self.pourcentage = 0
+        self.salleAssociee = salleAssociee
 
     def traiterImage(self):
         self.pourcentage = random.randint(0, 100)
 
     def detecterMouvement(self):
         detection = self.pourcentage > self.seuil
-        self.notifyObservers(detection)
+        self.notifyObservers(detection, self.salleAssociee)
 
 
-capPresence = CapteurPresence(30)
 appIoT = AppIoT()
+appSecu = AppSecu()
+salleA08 = Piece(8, 20)
+capPresence = CapteurPresence(30, salleA08)
+
 capPresence.addObserver(appIoT)
+capPresence.addObserver(appSecu)
 
 while True:
     capPresence.traiterImage()
